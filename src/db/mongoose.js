2035513b21daffd8939bf9490d3cb52101c28ba2
samirbsh <samirkumar2527@gmail.com>
2020-09-16 09:30:16 +0530

    sanitization, validation and cleaning

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,18 +9,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User',{
     name:{
-        type: String
+        type: String,
+        required:true,
+        trim:true
     },
     email:{
         type: String,
         required:true,
+        trim:true,
+        lowercase:true,
         validate(value){
         if(!validator.isEmail(value)){
             throw new Error('Email is invalid');
         }}
     },
+    password:{
+        type:String,
+        required:true,
+        minlength:7,
+        trim:true,
+        validate(value){
+            if(value.toLowerCase().includes("password")){
+                throw new Error('Password cannot contain "password"' )
+            }
+
+        }
+    },  
     age:{
         type: Number,
+        default:0,
         validate(value){
             if(value<0){
                 throw new Error('Age must a postive Number')                   
@@ -29,9 +46,11 @@ const User = mongoose.model('User',{
     }
 })
 // making an instance of it
+/*
 const me = new User({
-    name: 'Samir',
-    age:7
+    name: '    Samir    ',  
+    email:'          MYEMAIL@example.com          ',
+    password:'sasad@3434'
 })
 // saving the model
 me.save().then(()=>{
@@ -39,23 +58,27 @@ me.save().then(()=>{
 }).catch((error)=>{
     console.log(error)
 })
+*/
 
 //Setting Custom Validation
-//const task = mongoose.model('task',{
-//     description:{
-//         type: String
-//     },
-//     status:{
-//         type: Boolean
-//     }
-// })
-// const me = new task({
-//     description:'Cleaning the Car',
-//     status: 'true'
-// })
+const task = mongoose.model('task',{
+    description:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    status:{
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+const me = new task({
+    description:'  Studying         ',
+})
 
-// me.save().then(()=>{
-//     console.log(me)
-// }).catch((error)=>{
-//     console.log(error)
-// })
+me.save().then(()=>{
+    console.log(me)
+}).catch((error)=>{
+    console.log(error)
+})
