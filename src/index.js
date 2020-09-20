14468461c2d78faa435c74236cafa6d1a433d6fb
samirbsh <samirkumar2527@gmail.com>
2020-09-20 22:38:04 +0530

    Express MiddleWare

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // data is parsed from json to an object
+app.use((req, res, next) => {
+  //next registers middleware
+  res.status(503).send('site is currently down. Check back soon!')
+})
 app.use(express.json());
-
 app.use(userRouter)
 app.use(taskRouter)
-
 
 app.get("*", (req, res) => {
   res.send("<h1>404!</h1>");
@@ -37,3 +39,6 @@ const myFunctions = async () => {
 }
 
 myFunctions()
+
+// without middleware: new request -> run route handler
+//with middleware: new request -> do something -> run route handler
