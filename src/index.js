7b0811f9ce9dedc1cc6a465616d11f7998b6829f
samirbsh <samirkumar2527@gmail.com>
2020-09-21 06:45:56 +0530

    Hiding Private Data

--- src/index.js
+++ src/index.js
@@ -25,20 +25,5 @@ app.listen(port, () => {
   console.log("Server is listening on port " + port);
 });
 
-
-
-const myFunctions = async () => {
-  //Data that will be embedded in our token
-  //second argument (secret) this is used to sign the token making sure that it is
-  //not tampered or altered in any way
-  //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJkd2RldyIsImlhdCI6MTYwMDU4NDgyMH0.p8dmmSwS4IDXs3ZhCgJRbJuC_XLDwtVDvijcw75ka-Y
-  //header(type of token and algorithm used to generate) + payload(body) + signature(for verification) 
-  const token = jwt.sign({_id:'dwdew'},'newSecretkey',{expiresIn:'7 days'})
-  const data = jwt.verify(token,'newSecretkey')
-  console.log(data)
-}
-
-myFunctions()
-
 // without middleware: new request -> run route handler
 //with middleware: new request -> do something -> run route handler
