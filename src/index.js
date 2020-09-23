2d093566425c3b44106eb6542d487c0410112f9b
samirbsh <samirkumar2527@gmail.com>
2020-09-23 06:17:07 +0530

    auto cropping and image formatting

--- src/index.js
+++ src/index.js
@@ -24,30 +24,6 @@ app.get("*", (req, res) => {
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
 });
-
-const multer = require('multer')
-const upload = multer({ 
-  dest:'images',
-  limits:{
-    fileSize:1000000,
-  },
-  fileFilter(req,file,cb){
-    if(!(file.originalname.match(/\.(doc|docx)$/))){
-      return cb(new Error('Please upload a Word Document'))
-    }
-    cb(undefined,true)
-    // cb(new Error('File must be a pdf'))
-    // cb(undefined,true)
-    // cb(undefined,false)
-  }
-})
-
-app.post('/upload', upload.single('upload'),(req, res,next) =>{
-  res.send()
-},(error, req,res,next)=>{
-  res.status(400).send({Error: error.message});
-})
-
 // without middleware: new request -> run route handler
 //with middleware: new request -> do something -> run route handler
 
