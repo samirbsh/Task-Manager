76bb849763ebdf77ba8c7e3c51e7b44b89c1fdf1
samirbsh <samirkumar2527@gmail.com>
2020-09-24 08:27:47 +0530

    Testing Asynchronous Code

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip} = require('../src/math')
+const {calculateTip, add} = require('../src/math')
 
 test('Should calculate total with tip',() =>{
     const total = calculateTip(10,.3)
@@ -14,4 +14,24 @@ test('Should calculate total with default tip',() =>{
     // if(total !==13){
     //     throw new Error('Total tip should be 13. Got '+total)
     // }
+})
+
+// test('Async test demo',(done)=>{        //Pass parameters for async functions
+//     setTimeout(()=>{
+//         expect(1).toBe(2)
+//         done()
+//     },2000)
+// })
+
+test('Add 2 Numbers',(done)=>{
+    add(2,3).then((sum)=>{
+        expect(sum).toBe(5)
+        done()
+    })
+    
+})
+
+test('Add 2 Numbers async/await',async ()=>{
+    const sum = await add(10,22)
+    expect(sum).toBe(32)
 })
